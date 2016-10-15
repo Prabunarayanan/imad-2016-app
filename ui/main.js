4deb5928de7480d87d8e0ababcf6f3211036b905
Prabunarayanan <2147prabu@gmail.com>
2016-10-15 14:32:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,12 @@
 console.log('Loaded!');
 
+// Change the text of the main-text div
+var element = document.getElementById('main-text');
+
+element.innerHTML = 'This page is under Construction';
+
+//move the image
+var img = document.getElementById('hasuran');
+img.onclick = function () {
+    img.style.marginLeft = '100px';
+    };
