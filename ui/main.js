37f2c804f1abae4aa59324b47f70fd4ed4057562
Prabunarayanan <2147prabu@gmail.com>
2016-10-15 14:42:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,11 @@ element.innerHTML = 'Prabunarayanan Welcomes you to hasura Kingdom. \n This page
 
 //move the image
 var img = document.getElementById('hasuran');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight, 100);
     };
