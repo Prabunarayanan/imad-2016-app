eab6041c5a039156121b66c1d9390fe923c23f76
Prabunarayanan <2147prabu@gmail.com>
2016-10-15 14:27:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,2 @@
 console.log('Loaded!');
 
-// Change the text of the main-text div
-var element = document.getElementById('main-text');
-
-element.innerHTML = 'New value';
-
-//move the image
-var img = document.getElementById('hasuran');
-img.onclick = function () {
-    img.style.marginLeft = '100px';
-    };
