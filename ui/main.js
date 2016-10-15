91d4cb761e7c5217448efee01c2d386f7883ab4d
Prabunarayanan <2147prabu@gmail.com>
2016-10-15 11:16:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,6 @@ element.innerHTML = 'New value';
 
 //move the image
 var img = document.getElementById('img');
-img.oneclick = function () {
+img.onclick = function () {
     img.style.marginLeft = '100px';
     };
