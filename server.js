4af68b64a2557254a85f0f29a7ddbc0159282851
Prabunarayanan <2147prabu@gmail.com>
2016-10-22 12:41:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,10 +95,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter = 0;
-app.get('/counter',function (req,res) {
-    counter = counter + 1;
-    res.send(counter.toString())
+var ping = 0;
+app.get('/ping',function (req,res) {
+    counter = ping + 1;
+    res.send(ping.toString())
 });
 
 var names = [];
