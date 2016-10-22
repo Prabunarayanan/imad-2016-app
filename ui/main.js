72517ebd5c38ab189f47d79bd0c517d82fb7f58d
Prabunarayanan <2147prabu@gmail.com>
2016-10-22 09:29:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ button.onclick = function() {
     };
     
     // Make the request
-    request.open('GET', "http://prabunarayanan.imad.hasura-app.io",true);
+    request.open('GET', "http://prabunarayanan.imad.hasura-app.io/counter",true);
     request.send(null);
 };
 
