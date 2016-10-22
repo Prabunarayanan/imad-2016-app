7baf404a097ee526befbdcdf5cd35d0bfce0c524
Prabunarayanan <2147prabu@gmail.com>
2016-10-22 08:56:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,16 +11,21 @@ button.onclick = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             // Take some action
             if (request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                 //Capture a list of names and render it as a list
+                    var names = ['name1', 'name2', 'name3','name4'];
+                    var list = '';
+                    for (var i=0; i< names.length; i++) {
+                        list += '<li>' + names[i] + '</li>'; 
+                    }
+                     
+                
             }
         }
         // Not done yet
     };
     
     // Make the request
-    request.open('GET', 'http://prabunarayanan.imad.hasura-app.io/counter',true);
+    request.open('GET', 'http://prabunarayanan.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
 };
 
@@ -30,12 +35,5 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     //Make a request to the server and send the name
-    //Capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3','name4'];
-    var list = '';
-    for (var i=0; i< names.length; i++) {
-        list += '<li>' + names[i] + '</li>'; 
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+   
 };
