331bb4c39200c609d27db612bbf5f19699356a0c
Prabunarayanan <2147prabu@gmail.com>
2016-10-22 09:14:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,138 +1,58 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
+// Counter code
+var button = document.getElementBy Id('counter');
 
-var app = express();
-app.use(morgan('combined'));
-
-
-var articles = {
-  'article-one' : {
-  title: 'Article one | prabunarayanan',
-  heading: 'Article one',
-  date: 'February 14 1996',
-  content: ` <p>
-                   This is article one
-                </p>
-                <p>
-                    This is me Prabu
-                </p>
-                <p>
-                    minion
-                </p>`
-},
-  'article-two' : {
-      title: 'Article two | prabunarayanan',
-      heading: 'Article two',
-      date: 'February 14 2016',
-      content: ` <p>
-                       This is article two
-                    </p>
-                    <p>
-                        This is me yogi
-                    </p>
-                    <p>
-                        minion 
-                    </p>`
-      
-  },
-  'article-three' : {
-      title: 'Article Three | prabunarayanan',
-      heading: 'Article Three',
-      date: 'February 14 2017',
-      content: ` <p>
-                       This is article three
-                       Romeo & juliet
-                    </p>
-                    <p>
-                        Buddy boy
-                    </p>
-                    <p>
-                        minion = Kundhani
-                    </p>`
-      
-  },
+button.onclick = function() { 
+    
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if(request.readyState ===XMLHttpRequest.DONE) {
+            //Take sone action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // Not done yet
+    };
+    
+    // Make the request
+    request.open('GET', "http://prabunarayanan.imad.hasura-app.io",true);
+    request.send(null);
 };
 
-function createTemplate (data) { 
-    var title = data.title;
-    var date = data.date;
-    var heading = data.heading;
-    var content = data.content;
-        var htmlTemplate = `
-        <html>
-            <head>
-                <title>
-                    ${title}
-                </title>
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link href="/ui/style.css" rel="stylesheet" />
-            </head>
-           
-            <body>
-                <div class="container">
-                <div>
-                    <a href="/">Home</a>
-                </div>
-                <hr/>
-                <h3>
-                   ${heading}
-                </h3>
-                <div>
-                    ${date}
-                </div>
-                    <div>
-                        ${content}
-                    </div>
-                </div>
-            </body>
-        </html>
-        `;
-        return htmlTemplate;
+// Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function (){
+    // Make a request to the server and send the name
+    
+    // Capture a list of names and render it as a list
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for (var i=0; i< names.length; i++){
+        list += '<li>' + names[i] + '</li>';
+    }
+    var ul
 }
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-var counter = 0;
-app.get('/counter',function (req,res) {
-    counter = counter + 1;
-    res.send(counter.toString())
-});
-
-var names = [];
-app.get('/submit-name', function(req,res) { // URL: /submit-name?name=xxxxxx 
-    // Get the name from the request
-    var name = req.query.name;
-    
-    names.push(name);
-    // JSON: Javascript Object Notation
-    res.send(JSON.stringify(names));
-});
-
-app.get('/:articleName' , function (req, res) {
-    //articleName == article-one
-    //articles[articleName] == {} content object of aticle one
-   var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
-});
 
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
 
 
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+
+
+
+
+
+
+
+
+
+
+
+
